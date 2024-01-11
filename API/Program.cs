198a0e7b716d6cea3dc5750e25d4c6a1b4752e93
andersonandreas <adde1990@live.se>
2024-01-11 13:35:55 +0100

    fixed the error with updated db command . Todo for later is to fix the hardcoded connectionstring

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,6 @@
-using Microsoft.EntityFrameworkCore;
 using Database.Data;
 using Database.Handlers;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace API
@@ -10,17 +10,21 @@ namespace API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            string connectionString = builder.Configuration.GetConnectionString("MusicContext");
-            builder.Services.AddDbContext<MusicContext>(opt => opt.UseSqlServer(connectionString));
+
+            //string connectionString = builder.Configuration.GetConnectionString("dbConnection");
+
+            string hardcodedConnection = "Data Source=(localdb)\\.;Initial Catalog=MusicApiProjectDb;Integrated Security=True;Pooling=False;Trust Server Certificate=False";
+
+            builder.Services.AddDbContext<MusicContext>(opt => opt.UseSqlServer(hardcodedConnection));
 
             var app = builder.Build();
-            
+
 
             //Hämta alla personer i systemet
-            app.Map("/users",DbHelper.ListAllUserAsync);
+            app.Map("/users", DbHelper.ListAllUserAsync);
 
 
-            
+
             app.Run();
         }
     }
--- Database/Data/MusicContext.cs
+++ Database/Data/MusicContext.cs
@@ -1,19 +1,14 @@
 ﻿using Database.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Database.Data
 {
     public class MusicContext : DbContext
     {
-        public MusicContext(DbContextOptions<MusicContext> options):base(options)
-        {
-            
-        }
+
+        public MusicContext() { }
+        public MusicContext(DbContextOptions<MusicContext> options) : base(options) { }
+
         public DbSet<Artist> Artists { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<User> Users { get; set; }
@@ -22,7 +17,13 @@ namespace Database.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\.;Initial Catalog=MusikAPI;Integrated Security=True;Pooling=False");
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\.;Initial Catalog=MusicApiProjectDb;Integrated Security=True;Pooling=False;Trust Server Certificate=False");
         }
     }
 }
+
+
+// Alex connectionstring = "Data Source=(localdb)\\.;Initial Catalog=MusikAPI;Integrated Security=True;Pooling=False"
+
+// Andreas connectionstring = "Data Source=(localdb)\\.;Initial Catalog=MusicApiProjectDb;Integrated Security=True;Pooling=False;Trust Server Certificate=False"
+
