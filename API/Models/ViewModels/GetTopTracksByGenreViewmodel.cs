4e6641961654a072247097ed37a6078c5e228a60
Alexander Lundquist <knuta@DESKTOP-JU32ULF>
2024-01-16 10:01:52 +0100

    La till externa apier

---
+++ API/Models/ViewModels/GetTopTracksByGenreViewmodel.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace API.Models.ViewModels
+{
+    public class GetTopTracksByGenreViewmodel
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+    }
+}
