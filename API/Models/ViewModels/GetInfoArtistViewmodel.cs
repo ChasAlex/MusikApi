55a49074e55d6cb6d0020b8837a4daae7e823231
ChasAlex <alexander.lundquist@chasacademy.se>
2024-01-18 16:24:34 +0100

    Revert "Revert "La till externa apier""
    This reverts commit e8ffe6d8e89ed4340fb997b289b5c6ed862e43dd.

---
+++ API/Models/ViewModels/GetInfoArtistViewmodel.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace API.Models.ViewModels
+{
+    public class GetInfoArtistViewmodel
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("playcount")]
+        public string Playcount { get; set; }
+
+        [JsonPropertyName("summary")]
+        public string Summary { get; set; }
+    }
+}
--- Database/Handlers/DbHelper.cs
+++ Database/Handlers/DbHelper.cs
@@ -1,6 +1,9 @@
 ﻿using Database.Data;
 using Database.Models;
+
 using Microsoft.EntityFrameworkCore;
+
+
 
 namespace Database.Handlers
 {
@@ -18,11 +21,9 @@ namespace Database.Handlers
 
 
 
-        //public static async Task<IReadOnlyList<Genre>> ListAllGenresAsync(MusicContext context, int id)
-        //{
-        //    var results = context.Users.ToListAsync();
-        //    return await results;
-        //}
+        
+
+        
 
 
 
@@ -47,11 +48,11 @@ namespace Database.Handlers
 
         //// Koppla en person till en ny genre, artist och låt // Todo fix name
 
-        //public static async Task<IReadOnlyList<User>> ConntectPerson(MusicContext context)
-        //{
-        //    var results = context.Users.ToListAsync();
-        //    return await results;
-        //}   
+        // public static async Task ConnectUserToSong(MusicContext context, string id, CreateSongDTO dto)
+        // {
+
+
+        //}
 
 
 
