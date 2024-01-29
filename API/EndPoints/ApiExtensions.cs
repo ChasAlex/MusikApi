57a50d4c11bbdb6f8a0e4f1bba60e0629478431c
mlvestlund <mariavestlund@yahoo.se>
2024-01-30 00:01:32 +0100

    Added usersong and usergenre method and made overall changes
    Added methods to create new favorite artists and favorite genres, sorted the methods, wrote some comments and changed names of some methods.

--- API/EndPoints/ApiExtensions.cs
+++ API/EndPoints/ApiExtensions.cs
@@ -26,15 +26,28 @@ namespace API.EndPoints
     {
         public static IEndpointRouteBuilder MusicApiExtensions(this IEndpointRouteBuilder musicApi)
         {
+            //Gets all registered users
+            musicApi.Map("api/users", async (IPersonRepo repo) =>
+            {
+                var users = await repo.GetAllUsersAsync();
 
+                if (users == null || !users.Any())
+                {
+                    return Results.NotFound();
+                }
+
+                return Results.Ok(Handler.CreateUserDto(users));
+            });
+
+            //Login: Checks for matching credentials and retrieves connected users info
             musicApi.MapPost("/login", async (IPersonRepo repo, LoginCredential loginCredential) =>
             {
-                var loggedInUser = await repo.GetUserByCredentials(loginCredential.Username, loginCredential.Password);
+                var loggedInUser = await repo.GetUserByCredentialsAsync(loginCredential.Username, loginCredential.Password);
                 return loggedInUser;
             });
 
 
-            //work in progress for signup
+            //Signup: Checks if username is free and if so creates a new user
             musicApi.MapPost("/signup", async (IPersonRepo repo, SignupInfo signupInfo) =>
             {
                 string fullname = signupInfo.Fullname;
@@ -42,7 +55,7 @@ namespace API.EndPoints
                 string password = signupInfo.Password;
                 try
                 {
-                    await repo.CreateNewUser(fullname, username, password);
+                    await repo.CreateNewUserAsync(fullname, username, password);
                     return Results.StatusCode((int)HttpStatusCode.Created);
                 }
                 catch (InvalidOperationException ex)
@@ -57,36 +70,10 @@ namespace API.EndPoints
                 }
             });
 
-
-            musicApi.Map("api/users", async (IPersonRepo repo) =>
-            {
-                var users = await repo.GetAllUsers();
-
-                if (users == null || !users.Any())
-                {
-                    return Results.NotFound();
-                }
-
-                return Results.Ok(Handler.CreateUserDto(users));
-            });
-
-
-            musicApi.Map("api/genres/{id}", async (IPersonRepo repo, int id) =>
-            {
-                var genres = await repo.GetAllGenresByPersonId(id);
-
-                if (genres == null || !genres.Any())
-                {
-                    return Results.NotFound();
-                }
-
-                return Results.Ok(Handler.CreateGenreDto(genres));
-            });
-
-
+            //Gets all users favorited artists
             musicApi.Map("api/artists/{id}", async (IPersonRepo repo, int id) =>
             {
-                var artists = await repo.GetAllArtistsByPersonId(id);
+                var artists = await repo.GetAllArtistsByUserIdAsync(id);
 
                 if (artists == null || !artists.Any())
                 {
@@ -96,23 +83,10 @@ namespace API.EndPoints
                 return Results.Ok(Handler.CreateArtistDto(artists));
             });
 
-
-            musicApi.Map("api/artists/notconnected/{id}", async (IPersonRepo repo, int id) =>
-            {
-                var artists = await repo.GetAllArtistsNotConnectedByPersonId(id);
-
-                if (artists == null || !artists.Any())
-                {
-                    return Results.NotFound();
-                }
-
-                return Results.Ok(Handler.CreateArtistDto(artists));
-            });
-
-
+            //Gets all users favorited songs
             musicApi.Map("api/songs/{id}", async (IPersonRepo repo, int id) =>
             {
-                var songs = await repo.GetAllSongsByPersonId(id);
+                var songs = await repo.GetAllSongsByUserIdAsync(id);
 
                 if (songs == null || !songs.Any())
                 {
@@ -122,6 +96,57 @@ namespace API.EndPoints
                 return Results.Ok(Handler.CreateSongDto(songs));
             });
 
+            //Gets all users favorited genres
+            musicApi.Map("api/genres/{id}", async (IPersonRepo repo, int id) =>
+            {
+                var genres = await repo.GetAllGenresByUserIdAsync(id);
+
+                if (genres == null || !genres.Any())
+                {
+                    return Results.NotFound();
+                }
+
+                return Results.Ok(Handler.CreateGenreDto(genres));
+            });
+
+            //Gets all artists not favorited by user (yet)
+            musicApi.Map("api/artists/notconnected/{id}", async (IPersonRepo repo, int id) =>
+            {
+                var artists = await repo.GetAllArtistsNotConnectedByUserIdAsync(id);
+
+                if (artists == null || !artists.Any())
+                {
+                    return Results.NotFound();
+                }
+
+                return Results.Ok(Handler.CreateArtistDto(artists));
+            });
+
+            //Gets all songs not favorited by user (yet)
+            musicApi.Map("api/songs/notconnected/{id}", async (IPersonRepo repo, int id) =>
+            {
+                var songs = await repo.GetAllSongsNotConnectedByUserIdAsync(id);
+
+                if (songs == null || !songs.Any())
+                {
+                    return Results.NotFound();
+                }
+
+                return Results.Ok(Handler.CreateSongDto(songs));
+            });
+
+            //Gets all genres not favorited by user (yet)
+            musicApi.Map("api/genres/notconnected/{id}", async (IPersonRepo repo, int id) =>
+            {
+                var genres = await repo.GetAllGenresNotConnectedByUserIdAsync(id);
+
+                if (genres == null || !genres.Any())
+                {
+                    return Results.NotFound();
+                }
+
+                return Results.Ok(Handler.CreateGenreDto(genres));
+            });
 
             //Adds new connection between user and artist
             musicApi.Map("/userartist", async (IPersonRepo repo, UserArtist userArtist) =>
@@ -150,7 +175,7 @@ namespace API.EndPoints
 
         public static IEndpointRouteBuilder ExternalApiMusic(this IEndpointRouteBuilder musicApi)
         {
-            //Hämtar top låtar från en specifik artist
+            //Gets top songs for a chosen artist
             musicApi.MapGet("/artist/{artist}", async (string artist, [FromServices] IMusicServices musicServices) =>
             {
 
@@ -166,7 +191,7 @@ namespace API.EndPoints
 
             });
 
-            // Hämta toplåtar för en genre/Tag
+            //Gets top songs for a genre/tag
             musicApi.MapGet("/genre/{genre}", async (string genre, [FromServices] IMusicServices musicServices) =>
             {
 
@@ -182,7 +207,7 @@ namespace API.EndPoints
 
             });
 
-            //Hämta Bio,name och playcount for en artist
+            //Get bio, name and playcount for an artist
             musicApi.MapGet("/artistinfo/{artist}", async (string artist, [FromServices] IMusicServices musicServices) =>
             {
 
@@ -200,9 +225,5 @@ namespace API.EndPoints
 
             return musicApi;
         }
-
-
-
-
     }
 }
